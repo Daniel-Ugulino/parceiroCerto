9586622ec91d196bba364bf6a2f277763ea3c0b2
danielugulino <d.ugulino@picalike.com>
2024-09-09 18:41:27 -0300

    feat: test

--- ServerRegistry/build.sh
+++ ServerRegistry/build.sh
@@ -1,5 +1,5 @@
 export EUREKAHOST=localhost
-./mvnw clean package
+./ApiGateway/mvnw clean package
 docker build -t serverregistry:latest .
 docker tag serverregistry:latest danielugulino/serverregistry:latest
 docker push danielugulino/serverregistry:latest
