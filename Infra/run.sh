c96020cc9d3d666f9273f85fde40e2205fc7368f
danielugulino <d.ugulino@picalike.com>
2024-09-09 23:57:38 -0300

    feat: updated

--- Infra/run.sh
+++ Infra/run.sh
@@ -1,20 +1,30 @@
 kubectl apply -f postgis.yml
+echo 'RUNNING postgis '
 sleep 30
 kubectl apply -f rabbitMq.yml
+echo 'RUNNING rabbitMq '
 sleep 10
 kubectl apply -f serverRegistry.yml
+echo 'RUNNING serverRegistry '
 sleep 10
 kubectl apply -f feedbackService.yml
+echo 'RUNNING feedbackService '
 sleep 10
 kubectl apply -f bff.yml
+echo 'RUNNING bff '
 sleep 10
 kubectl apply -f authService.yml
+echo 'RUNNING authService '
 sleep 10
 kubectl apply -f userService.yml
+echo 'RUNNING userService '
 sleep 40
 kubectl apply -f taskService.yml
+echo 'RUNNING taskService '
 sleep 40
 kubectl apply -f chatService.yml
+echo 'RUNNING chatService '
 sleep 10
 kubectl apply -f apiGateway.yml
+echo 'running apiGateway '
 sleep 20
