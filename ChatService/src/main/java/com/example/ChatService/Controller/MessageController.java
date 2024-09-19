2403779e490c554a2873c26c53c2383196775f7c
danielugulino <d.ugulino@picalike.com>
2024-09-19 19:24:43 -0300

    feat: removed webscoket connection because of kubernetes

--- ChatService/src/main/java/com/example/ChatService/Controller/MessageController.java
+++ ChatService/src/main/java/com/example/ChatService/Controller/MessageController.java
@@ -1,20 +1,16 @@
 package com.example.ChatService.Controller;
 
 import com.example.ChatService.Domain.Message;
-import com.example.ChatService.Dto.ChatDto;
 import com.example.ChatService.Dto.MessageDto;
-import com.example.ChatService.Dto.UserDto;
 import com.example.ChatService.Service.MessageService;
-import com.fasterxml.jackson.core.JsonProcessingException;
+import com.example.ChatService.Utils.CustomResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping("/message")
@@ -22,21 +18,16 @@ public class MessageController {
     @Autowired
     MessageService messageService;
 
-    private final ObjectMapper objectMapper = new ObjectMapper();
-
-    @MessageMapping("/register")
-    @SendTo("/topic/message")
-    public UserDto enterChat(String messageString, SimpMessageHeaderAccessor headerAccessor) throws JsonProcessingException {
-        UserDto userDto = objectMapper.readValue(messageString, UserDto.class);
-        headerAccessor.getSessionAttributes().put("username", userDto.getSender());
-        return userDto;
-    }
-
-    @MessageMapping("send")
-    @SendTo("/topic/public")
-    public String sendMessage(String messageString, SimpMessageHeaderAccessor headerAccessor) throws JsonProcessingException {
-        MessageDto messageDto = objectMapper.readValue(messageString, MessageDto.class);
-        messageService.save(messageDto);
-        return messageDto.getContent();
+    @PostMapping()
+    public ResponseEntity<Object> sendMessage(MessageDto messageDto){
+        try {
+            Message messageEntity = messageService.save(messageDto);
+            return ResponseEntity.status(HttpStatus.OK).body(new CustomResponse<>("Message Sent Successfully",messageEntity));
+        } catch (DataIntegrityViolationException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new CustomResponse<>("Data integrity violation"));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Internal server error");
+        }
     }
 }
